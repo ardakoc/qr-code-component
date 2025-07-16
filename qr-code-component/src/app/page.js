6e45ee01d7839b1a89eddafeed1392a712e81087
Arda Koç <ardaskoc@gmail.com>
2025-07-16 23:32:25 +0300

    added text and completed component.

--- qr-code-component/src/app/page.js
+++ qr-code-component/src/app/page.js
@@ -2,9 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center content-center bg-slate-300">
-      <div className="bg-white rounded-2xl border-none flex justify-center">
-        <Image src="/image-qr-code.png" width={240} height={240} alt="Qr Code" className="m-4 rounded-lg" />
+    <div className="h-screen flex items-center justify-center content-center bg-slate-300 p-2">
+      <div className="w-68 bg-white rounded-2xl border-none flex flex-col justify-center items-center">
+        <div className="p-4">
+          <Image src="/image-qr-code.png" width={240} height={240} alt="Qr Code" className="rounded-lg" />
+        </div>
+        <div className="flex flex-col gap-y-3 text-center px-8 pb-8">
+          <h1 className="font-bold text-lg leading-5.5">Improve your front-end skills by building projects</h1>
+          <p className="text-gray-400 text-xs">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
       </div>
     </div>
   );
