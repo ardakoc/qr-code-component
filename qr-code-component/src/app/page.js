a6847c944d35d7a52cfe6065a60fa9d05367153e
Arda Koç <ardaskoc@gmail.com>
2025-07-16 23:12:17 +0300

    configured layout. added qr code to the component.

--- qr-code-component/src/app/page.js
+++ qr-code-component/src/app/page.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center content-center">
-      Hello
+    <div className="h-screen flex items-center justify-center content-center bg-slate-300">
+      <div className="bg-white rounded-2xl border-none flex justify-center">
+        <Image src="/image-qr-code.png" width={240} height={240} alt="Qr Code" className="m-4 rounded-lg" />
+      </div>
     </div>
   );
 }
