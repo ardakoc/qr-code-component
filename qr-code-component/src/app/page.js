17df09bd73a57f49fe4cd0c21a4265bea8cc8df1
Arda Koç <ardaskoc@gmail.com>
2025-07-17 12:01:48 +0300

    switched div with main.

--- qr-code-component/src/app/page.js
+++ qr-code-component/src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center content-center bg-slate-300 p-2">
+    <main role="main" className="h-screen flex items-center justify-center content-center bg-slate-300 p-2">
       <div className="w-68 bg-white rounded-2xl border-none flex flex-col justify-center items-center">
         <div className="p-4">
           <Image src="/image-qr-code.png" width={240} height={240} alt="Qr Code" className="rounded-lg" />
@@ -12,6 +12,6 @@ export default function Home() {
           <p className="text-gray-400 text-xs">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
